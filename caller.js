9b9cc9d22a145dc7306f859d379cb271a1315423
Aquib <85285702+Aquib100@users.noreply.github.com>
2021-11-10 15:09:40 +0530

    Update caller.js

--- caller.js
+++ caller.js
@@ -1,1 +1,11 @@
+var phoneNumber = document.getElementById("input").value;
 
+function call(){
+  navigator.mediaDevices.getUserMedia({ audio: true})
+   .then(function(stream) {
+        window.open("tel:" + phoneNumber, "_self");
+      })
+      .catch(function(err) {
+        window.alert("Please grant the microphone permission !");
+      });
+}
